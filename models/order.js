75b456df5b9d26af0bf535db51fc3fdf71ea9dc8
gnn <honeyhomie@gmail.com>
2020-02-24 13:52:31 -0800

    fixed type errors

--- models/order.js
+++ models/order.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
-var OrderSchema = new.mongoose.Schema({
-    driver: {type: mongoose.Schema.Types.ObjectId, ref: "Driver"},
+var OrderSchema = new mongoose.Schema({
+    //driver: {type: mongoose.Schema.Types.ObjectId, ref: "Driver"},
     restuarants: [{type: mongoose.Schema.Types.ObjectId, ref: "Restaurant"}],
     orderItems: [{type: mongoose.Schema.Types.ObjectId, ref: "MenuItem"}],
     totalPrice: Number,
