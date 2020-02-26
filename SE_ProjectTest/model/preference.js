1e30e431ab0b4f02df9ead5e96312ce3df568969
Jenson Tran <JensonTran@b98-aruba-authenticated-10-110-185-85.wlan.cpp.edu>
2020-02-25 19:43:32 -0800

    removed dupe folder

--- SE_ProjectTest/model/preference.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require("mongoose");
-
-var PreferenceSchema = new mongoose.Schema({
-    foodCategories: [String],
-});
-
-module.exports = mongoose.model("Preference", PreferenceSchema);
