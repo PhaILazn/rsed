3b4e932e3d39e37994b5b44967bbdc34f177742b
PhaILazn <58123824+PhaILazn@users.noreply.github.com>
2020-02-27 11:14:27 -0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 //Connect to mongodb
-<<<<<<< HEAD:backend/index.js
-const URI = 'mongodb+srv://Jay:boob@bigodobonhonkeros-jdryx.mongodb.net/OmNom_Foods?retryWrites=true&w=majority';
-=======
-const URI = 'mongodb+srv://user:password@bigodobonhonkeros-jdryx.mongodb.net/OmNom_Foods?retryWrites=true&w=majority';
->>>>>>> fa934b8b01d52b151f6c09fbd2fb4713fb9e9fad:Backend_files/index.js
+const URI = 'mongodb+srv://<user:password>@bigodobonhonkeros-jdryx.mongodb.net/OmNom_Foods?retryWrites=true&w=majority';
+
 mongoose.connect(URI,
     {
         useNewUrlParser: true,
@@ -25,16 +22,11 @@ db.once('open', function() {
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-<<<<<<< HEAD:backend/index.js
 app.use("/getall", testingRoutes);
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 app.get("/testing",(req,res)=>res.send("This is a testing to see if server is connected..."));
 
-=======
-app.use("/", testingRoutes);
-app.get("/poop",(req,res)=>res.send('Hello World'));
->>>>>>> fa934b8b01d52b151f6c09fbd2fb4713fb9e9fad:Backend_files/index.js
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
