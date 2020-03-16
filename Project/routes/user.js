cc7fbc455745fdbb4788a6311897617f4470f36c
gnn <honeyhomie@gmail.com>
2020-03-16 12:39:53 -0700

    added user routes

---
+++ Project/routes/user.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const User = require('../models/user');
+const Address = require('../models/address');
+const Menu = require('../models/menu');
+const MenuItem = require('../models/menuitem');
+const Order = require('../models/order');
+const Preference = require('../models/preference');
+const Restaurant = require('../models/restaurant');
+const Review = require('../models/review');
+
+const router = express.Router();
+
+router.get('/',function(req, res) {
+    res.send("Test");
+});
+
+router.get('/:userId', function(req, res) {
+    var userId = req.params.userId;
+    //Find user by objectId
+    User.findById(userId)
+    .exec()
+    .then(currUser => {
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+    });
+    res.render("profile");
+});
+
+module.exports = router;
