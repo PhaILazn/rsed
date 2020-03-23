3dfde1e62daa1713843a95ea39fd795c990d390a
gnn <honeyhomie@gmail.com>
2020-03-23 14:03:45 -0700

    updated restaurantprofile.js

--- project/routes/restaurantprofile.js
+++ project/routes/restaurantprofile.js
@@ -10,6 +10,32 @@ const Review = require('../models/review');
 
 const router = express.Router();
 
+router.get('/:id',isLoggedIn, function(req, res) {
+    //Search db for user by ObjectId
+    Restaurant.findById(req.params.id, function(err,foundRestaurant){
+        if(err){
+            console.log(err)
+        }else{
+            foundRestaurant.populate('foodCategories');
+            foundRestaurant.populate('reviews');
+            var resMenu;
+            Menu.findById(foundRestaurant.menus[0], function (err, foundMenu) {
+                if(err) {
+                    console.log(err);
+                }else{
+                    resMenu = foundMenu;
+                }
+            });
+            res.render('restaurantprofile',{restaurant: foundRestaurant, menu: resMenu});
+        }
+    });
+});
 
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect("/signin");
+}
 
 module.exports = router;
