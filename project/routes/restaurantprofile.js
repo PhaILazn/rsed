85c81b84e2c07e83bfac07f6bb9d7180bbe2187f
gnn <honeyhomie@gmail.com>
2020-03-23 13:17:48 -0700

    added restaurantprofile.js

---
+++ project/routes/restaurantprofile.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const User = require('../models/user');
+const Address = require('../models/address');
+const Menu = require('../models/menu');
+const MenuItem = require('../models/menuitem');
+const Order = require('../models/order');
+const Preference = require('../models/preference');
+const Restaurant = require('../models/restaurant');
+const Review = require('../models/review');
+
+const router = express.Router();
+
+
+
+module.exports = router;
