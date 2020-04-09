9d0663e8fbd592d50d3d4d9d0b27411f1f0fdbeb
Grant <honeyhomie@gmail.com>
2020-04-09 12:25:01 -0700

    added restaurantId to menuitem schema

--- project/models/menuitem.js
+++ project/models/menuitem.js
@@ -4,6 +4,7 @@ var MenuItemSchema = new mongoose.Schema({
     name: String,
     price: Number,
     image: {type: String, default: "https://i.imgur.com/mvKmhcw.jpg"},
+    restaurant: {type: mongoose.Schema.Types.ObjectId, ref: "Restaurant"},
 },{
     versionKey: false
 });
