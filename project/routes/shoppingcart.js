8171b22a66a464aaaf626d9ce8231d2737ece5bb
Grant <honeyhomie@gmail.com>
2020-04-09 20:07:20 -0700

    added some stuff to shoppingcart.js

---
+++ project/routes/shoppingcart.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const User = require('../models/user');
+const Address = require('../models/address');
+const Menu = require('../models/menu');
+const MenuItem = require('../models/menuitem');
+const Order = require('../models/order');
+const Preference = require('../models/preference');
+const Restaurant = require('../models/restaurant');
+const Review = require('../models/review');
+const Cart = require('../models/cart');
+
+const router = express.Router();
+
+router.get('/', isLoggedIn, function(req, res) {
+    var menuItems = req.menuItems;
+    array.forEach(menuItems => {
+        
+    });
+    res.render(); 
+});
+
+router.get('/add/:id', isLoggedIn, function(req, res) {
+    var menuItemId = req.menuItemId;
+    var cart = new Cart(req.session.cart? req.session.cart : {});
+    MenuItem.findById(menuItemId, function(err, menuItem) {
+        if(err) {
+            return res.redirect('/');
+        }
+        else {
+            
+        }
+    });
+});
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect("/signin");
+}
+
+module.exports = router;
