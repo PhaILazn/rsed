0042785ea89d7a6127f80bc0b26233860369d141
Grant <honeyhomie@gmail.com>
2020-04-22 17:33:28 -0700

    remove item from shopping cart works

--- project/routes/shoppingcart.js
+++ project/routes/shoppingcart.js
@@ -10,7 +10,6 @@ const Review = require('../models/review');
 const Cart = require('../models/shoppingcart');
 
 const router = express.Router();
-const ShoppingCart = require('../models/shoppingcart');
 
 router.get('/', isLoggedIn, function(req, res) {
     Cart.findById(req.user.shoppingCart)
@@ -23,6 +22,33 @@ router.get('/', isLoggedIn, function(req, res) {
         }
         res.render('shoppingcart', {currentOrder: foundCart});
     });
+});
+
+//Route to remove item from a shopping cart
+router.get('/remove/:id', isLoggedIn, function(req, res) {
+    Cart.findById(req.user.shoppingCart)
+    .populate({
+        path: 'items',
+    })
+    .exec(function(err, foundCart) {
+        for(var i = 0; i < foundCart.items.length; ++i) {
+            console.log(foundCart.items[i]._id);
+            if(foundCart.items[i]._id == req.params.id) {
+                foundCart.totalPrice = foundCart.totalPrice - foundCart.items[i].price;
+                foundCart.items.splice(i,i + 1);
+                break;
+            }
+        }
+        foundCart.save();
+    });
+    // Cart.updateOne(
+    //     {"_id": req.user.shoppingCart},
+    //     {"$pull": {"items": req.params.id}},
+    //     function(err, status) {
+    //         console.log(status);
+    //     }
+    // );
+    res.redirect('/shoppingcart');
 });
 
 function isLoggedIn(req,res,next){
