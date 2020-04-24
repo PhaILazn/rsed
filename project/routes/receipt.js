c7e712e5de5709bcc2ba9d535d850fcf81ad025a
Taylor Pastor <tvpastor@cpp.edu>
2020-04-23 22:13:02 -0700

    Hi

--- project/routes/receipt.js
+++ project/routes/receipt.js
@@ -1,6 +1,6 @@
 const express = require("express");
-
-const Restaurant = require("../models/restaurant");
+const User = require("../models/user");
+var moment = require("moment");
 
 const router = express.Router();
 
@@ -8,19 +8,49 @@ const router = express.Router();
   res.render("receipt");
 });*/
 
-router.get("/", function (req, res) {
+/*router.get("/", function (req, res) {
   //Search db for user by ObjectId
-  Restaurant.find(req.params).populate("address").populate({
-    path: 'menus',
-    populate:{
-      path: "menuItems",
-    }
-  }).exec(function(err,foundRestaurant){
-    if(err){
-      res.redirect('/');
-    }
-    res.render('receipt',{restaurant: foundRestaurant})
-  })
+  Restaurant.find(req.params)
+    .populate("address")
+    .populate({
+      path: "menus",
+      populate: {
+        path: "menuItems",
+      },
+    })
+    .exec(function (err, foundRestaurant) {
+      if (err) {
+        res.redirect("/");
+      }
+      res.render("receipt", { restaurant: foundRestaurant });
+    });
+});*/
+
+router.get("/", isLoggedIn, function (req, res) {
+  User.findById(req.user)
+    .populate({
+      path: "orderHistory",
+      populate: {
+        path: "orderItems",
+      },
+    })
+    .exec(function (err, foundorder) {
+      if (err) {
+        res.redirect("/");
+      }
+      for (var i = 0; i < foundorder.orderHistory.length; i++) {
+        console.log(foundorder.orderHistory[i]);
+      }
+
+      res.render("receipt", { orderHistory: foundorder, moment: moment });
+    });
 });
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/signin");
+}
 
 module.exports = router;
